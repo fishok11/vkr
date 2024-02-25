594bfcaab47d6a66519225a9ed617496746cb526
fishok11 <svllsd811@gmail.com>
2024-02-25 15:30:29 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ArticlesPage from "./pages/ArticlesPage";
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
 
-
-export const App = () => {
+function App() {
   return (
-    <div>
-      <Routes>
-        <Route index element={<MainPage />}/>
-        <Route path={`/articles`} element={<ArticlesPage />}/>
-      </Routes>
-    </div>
+    <>
+      <Header />
+      <div className={styles.main}>
+        <Routes>
+          <Route index element={<MainPage />}/>
+          <Route path={`/articles`} element={<ArticlesPage />}/>
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+
+const Header: FC = () => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.linkContainer}>
+        <Link to={'/'} className={styles.link}>
+          Home
+        </Link>
+        <Link to={'/articles'} className={styles.link}>
+          Articles  
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
