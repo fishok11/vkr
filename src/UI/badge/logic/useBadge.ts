c239d8b21d404d5673c226d2d51a977507356149
fishok11 <svllsd811@gmail.com>
2024-03-17 21:49:28 +0300

    fix badge colors

--- src/UI/badge/logic/useBadge.ts
+++ src/UI/badge/logic/useBadge.ts
@@ -4,13 +4,13 @@ import styles from '../Badge.module.scss';
 export const useBadge = ({ grade }: { grade: number }) => {
   const [badgeColor, setBadgeColor] = useState('');
   useEffect(() => {
-    if (grade <= 30) {
+    if (grade < 50) {
       setBadgeColor(styles.red);
     }
-    if (grade > 30 && grade <= 50) {
+    if (grade >= 50 && grade <= 80) {
       setBadgeColor(styles.yellow);
     }
-    if (grade > 50) {
+    if (grade > 80) {
       setBadgeColor(styles.green);
     }
   }, [grade]);
